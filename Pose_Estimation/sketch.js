276dc1393ad7e5d20e2e86ab903f03745b6c9067
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-06-14 00:31:46 +0600

    Create sketch.js

---
+++ Pose_Estimation/sketch.js
@@ -0,0 +1,53 @@
+let video;
+let poseNet;
+let noseX = 0;
+let noseY = 0;
+let eyelX = 0;
+let eyelY = 0;
+let eyerX = 0;
+let eyerY = 0;
+ 
+function setup() {
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.hide();
+  poseNet = ml5.poseNet(video, modelReady);
+  poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(poses) {
+  // console.log(poses);
+  if (poses.length > 0) {
+    let nX = poses[0].pose.keypoints[0].position.x;
+    let nY = poses[0].pose.keypoints[0].position.y;
+    let eX = poses[0].pose.keypoints[1].position.x;
+    let eY = poses[0].pose.keypoints[1].position.y;
+    let EX = poses[0].pose.keypoints[2].position.x;
+    let EY = poses[0].pose.keypoints[2].position.y;
+    noseX = lerp(noseX, nX, 0.5);
+    noseY = lerp(noseY, nY, 0.5);
+    eyelX = lerp(eyelX, eX, 0.5);
+    eyelY = lerp(eyelY, eY, 0.5);
+    eyerX = lerp(eyerX, EX, 0.5);
+    eyerY = lerp(eyerY, EY, 0.5);
+  }
+}
+
+function modelReady() {
+  console.log('model ready');
+}
+
+function draw() {
+  image(video, 0, 0);
+  
+  let d = dist(noseX, noseY, eyelX, eyelY);
+
+  fill(255, 0, 0);
+  ellipse(noseX, noseY, d/3);
+  fill(0,0,255);
+  ellipse(eyelX, eyelY, d/3);
+  fill(0,0,255);
+  ellipse(eyerX, eyerY, d/3);
+
+
+}
