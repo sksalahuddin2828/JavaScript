558164eb39a481ac1cf683fcdf7f0925562768cf
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-15 13:34:46 +0600

    Create armstrong_number.js

---
+++ armstrong_number.js
@@ -0,0 +1,18 @@
+function isArmstrongNumber(n) {
+    const numDigits = String(n).length;
+    let sumOfPowers = 0;
+    let temp = n;
+    while (temp > 0) {
+        const digit = temp % 10;
+        sumOfPowers += Math.pow(digit, numDigits);
+        temp = Math.floor(temp / 10);
+    }
+    return n === sumOfPowers;
+}
+
+const user_input = prompt("Enter a number:");
+if (isArmstrongNumber(user_input)) {
+    console.log(`${user_input} is an Armstrong number.`);
+} else {
+    console.log(`${user_input} is not an Armstrong number.`);
+}
