f0d4e5089b85d5c7d9cfa8b9a6aed903c1a487bf
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-15 14:01:55 +0600

    Update armstrong_number.js

--- armstrong_number.js
+++ armstrong_number.js
@@ -12,7 +12,25 @@ function isArmstrongNumber(n) {
 
 const user_input = prompt("Enter a number:");
 if (isArmstrongNumber(user_input)) {
-    console.log(`${user_input} is an Armstrong number.`);
+    console.log('${user_input} is an Armstrong number.');
 } else {
-    console.log(`${user_input} is not an Armstrong number.`);
+    console.log('${user_input} is not an Armstrong number.');
 }
+
+//-----------------------------------------------------------------------
+
+function isArmstrongNumber(n) {
+    const numDigits = String(n).length;
+    let sumOfPowers = 0;
+    let temp = n;
+    while (temp > 0) {
+        const digit = temp % 10;
+        sumOfPowers += Math.pow(digit, numDigits);
+        temp = Math.floor(temp / 10);
+    }
+    return n === sumOfPowers;
+}
+
+console.log(isArmstrongNumber(153)); 
+
+// Output: true
