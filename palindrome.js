dc6a1e1d1e5ccbb8be164b6f164656f87231c06b
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-15 13:43:46 +0600

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -9,3 +9,14 @@ if (isPalindrome(user_input)) {
 } else {
     console.log(`'${user_input}' is not a palindrome.`);
 }
+
+//--------------------------------------------------------------
+
+function isPalindrome(s) {
+    s = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    return s === s.split('').reverse().join('');
+}
+
+console.log(isPalindrome("A man, a plan, a canal: Panama"));  
+
+// Output: true
