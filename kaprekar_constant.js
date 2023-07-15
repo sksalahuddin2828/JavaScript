bd0811cd79fdbd52888f090b1b7c2656e193b88d
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-15 14:09:58 +0600

    Update kaprekar_constant.js

--- kaprekar_constant.js
+++ kaprekar_constant.js
@@ -13,3 +13,21 @@ function kaprekarConstant(n) {
 const user_input = prompt("Enter a number:");
 const steps = kaprekarConstant(user_input);
 console.log(`Number of steps to reach Kaprekar constant: ${steps}`);
+
+//------------------------------------------------------------------------------------
+
+function kaprekarConstant(n) {
+    let count = 0;
+    while (n !== 6174) {
+        count++;
+        let digits = n.toString().padStart(4, '0');
+        let ascending = Number(digits.split('').sort().join(''));
+        let descending = Number(digits.split('').sort().reverse().join(''));
+        n = descending - ascending;
+    }
+    return count;
+}
+
+console.log(kaprekarConstant(1234));  
+
+// Output: 3
